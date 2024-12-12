0f2edf1e1f6cd536119de3e51dd9cdf273aaa345
CxForten <chrstfrchvrr14@gmail.com>
2024-12-11 23:50:43 -0500

    deploy3

--- src/app/services/expense.service.ts
+++ src/app/services/expense.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ExpenseService {
-  private readonly API_URL = 'http://127.0.0.1:8000/api'
+  private readonly API_URL = 'http://gastos-api-production.up.railway.app:8000/api'
 
   constructor(
     private http: HttpClient
