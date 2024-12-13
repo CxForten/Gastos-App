22b4b9f3a690b87104bbcee8a8e2256515a72965
CxForten <chrstfrchvrr14@gmail.com>
2024-12-13 07:13:10 -0500

    correctiom

--- src/app/services/expense.service.ts
+++ src/app/services/expense.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ExpenseService {
-  private readonly API_URL = 'http://gastos-api-production.up.railway.app/api'
+  private readonly API_URL = 'https://gastos-api-production.up.railway.app/api'
 
   constructor(
     private http: HttpClient
